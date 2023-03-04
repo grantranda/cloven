4141d41fe48a2b8657faff00c1653b4de85e9a0b
Grant Randa <48031522+grantranda@users.noreply.github.com>
2023-03-04 16:22:38 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,84 +4,25 @@
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
-#include "shader.h"
 
-static void glfw_error_callback(int error, const char* description) {
-	fprintf(stderr, "GLFW Error %d: %s\n", error, description);
-}
+#include "gui.h"
+#include "shader.h"
+#include "window.h"
+#include "windows/test_window.h"
 
 int main() {
-	glfwSetErrorCallback(glfw_error_callback);
-	if (!glfwInit()) {
-		return -1;
+	Window window("Cloven", 1920, 1080);
+
+	std::vector<std::unique_ptr<GuiWindow>> windows;
+	windows.push_back(std::make_unique<TestWindow>());
+	
+	const Gui gui(window.glfw_window, std::move(windows));
+
+	while (!window.should_close()) {
+		window.update();
+		window.render();
+		gui.render();
 	}
-
-	// Set OpenGL version
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-	const char* glsl_version = "#version 460";
-
-	// Create window
-	GLFWwindow* window = glfwCreateWindow(1920, 1080, "Cloven", nullptr, nullptr);
-	if (!window) {
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	// glfwSwapInterval(1); // Enable VSync
-
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-
-	// Initialize ImGui
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
-	(void)io;
-
-	// Set ImGui style
-	ImGui::StyleColorsDark();
-	// ImGui::StyleColorsLight();
-
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init(glsl_version);
-
-	constexpr ImVec4 clear_color = ImVec4(0.8f, 0.8f, 0.8f, 1.0f);
-
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-
-		// Start the ImGui frame
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
-
-		// Create ImGui windows
-		// ImGui::ShowDemoWindow();
-		{
-			ImGui::Begin("Title");
-
-			ImGui::End();
-		}
-
-		// Rendering
-		ImGui::Render();
-		int display_w, display_h;
-		glfwGetFramebufferSize(window, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
-		glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w,
-		             clear_color.w);
-		glClear(GL_COLOR_BUFFER_BIT);
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-		glfwSwapBuffers(window);
-	}
-
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
-
-	glfwDestroyWindow(window);
-	glfwTerminate();
 
 	return 0;
 }
