30b46dceb40d53ee820fbf940a72ae3cd3c7f3c4
Grant Randa <48031522+grantranda@users.noreply.github.com>
2023-05-02 12:31:30 -0400

    Update vertex

--- src/vertex.h
+++ src/vertex.h
@@ -3,7 +3,7 @@
 
 struct Vertex {
 	Vertex() = default;
-	Vertex(const float x, const float y, const float z): pos(x, y, z) {}
+	Vertex(const float x, const float y, const float z) : pos(x, y, z) {}
 	explicit Vertex(const glm::vec3 pos): pos(pos) {}
 	Vertex(const glm::vec3 pos, const glm::vec2 tex_coords): pos(pos), tex_coords(tex_coords) {}
 
